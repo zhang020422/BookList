ce1203b09e0375c597e2e8de446a694466b848c2
dingbianqi <672417929@qq.com>
2022-04-02 18:42:29 +0800

    999

--- Global.asax.cs
+++ Global.asax.cs
@@ -15,6 +15,7 @@ namespace BookList
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            111111111111111111111111
         }
     }
 }
