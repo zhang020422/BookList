52d3becfa3a4510e2a516b48c2ef1f2d3e4c28be
花酒 <619706462@qq.com>
2022-04-02 16:49:05 +0800

    添加项目文件。

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookList.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string isbn { get; set; }
+        public string bookName { get; set; }
+        public string author { get; set; }
+        public string press { get; set; }
+        public DateTime pressDate { get; set; }
+        public double price { get; set; }
+        public string barcode { get; set; }
+        public string size { get; set; }
+        public double weight { get; set; }
+        public int format { get; set; }
+    }
+}
---
+++ Models/BookDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookList.Models
+{
+    public class BookDBContext:DbContext
+    {
+      public DbSet<Book> books { get; set; }
+    }
+}
